28029a58247a5f149351af978703f4202060a7fc
aeons <jayden_gunhan@hotmail.com>
2023-10-03 21:00:33 +0200

    Project Refactor

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import { router } from "@/router";
-import App from '../AppEWA.vue'
+import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 createApp(App).use(router).mount('#app')
+
+import 'bootstrap/dist/js/bootstrap.bundle.js'
--- frontend/src/models/product.js
+++ frontend/src/models/product.js
@@ -1,7 +1,8 @@
 export class product{
-    constructor(id, name, quantity) {
+    constructor(id, name, description, quantity) {
         this.id = id;
         this.name = name;
+        this.description = description;
         this.quantity = quantity;
     }
 
