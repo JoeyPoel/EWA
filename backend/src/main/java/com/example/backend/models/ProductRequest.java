63552cdb7ba3ba0d447ab61d9ccae30b4ea083aa
Joey van der Poel <Joey.van.der.poel@gmail.com>
2023-11-02 19:02:23 +0100

    More changes, keep having issues with CORS

---
+++ backend/src/main/java/com/example/backend/models/ProductRequest.java
@@ -0,0 +1,52 @@
+package com.example.backend.models;
+
+import javax.print.DocFlavor;
+import java.util.List;
+
+public class ProductRequest {
+    private int warehouseId;
+    private String description;
+    private int quantity;
+    private String name;
+    private List<Warehouse> warehouses;
+
+    public List<Warehouse> getWarehouses() {
+        return warehouses;
+    }
+
+    public void setWarehouses(List<Warehouse> warehouses) {
+        this.warehouses = warehouses;
+    }
+
+    public int getWarehouseId() {
+        return warehouseId;
+    }
+
+    public void setWarehouseId(int warehouseId) {
+        this.warehouseId = warehouseId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- backend/src/main/java/com/example/backend/rest/ProductController.java
+++ backend/src/main/java/com/example/backend/rest/ProductController.java
@@ -1,17 +1,20 @@
 package com.example.backend.rest;
 import com.example.backend.models.Product;
+import com.example.backend.models.ProductRequest;
+import com.example.backend.models.Warehouse;
 import com.example.backend.repositories.ProductRepository;
 import com.example.backend.repositories.ProductRepositoryMock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/products")
-@CrossOrigin("http://localhost:8080/")
 public class ProductController {
 
     @Autowired
@@ -35,12 +38,40 @@ public class ProductController {
     }
 
     @PostMapping("/addProduct")
-    public ResponseEntity<String> addProduct(@RequestBody Product product) {
+    public ResponseEntity<String> addProduct(@RequestBody ProductRequest productRequest) {
         try {
-            productRepository.AddProduct(1,product.getName(),0, 0 ); // Save the product to the database
-            return new ResponseEntity<>("Product added successfully", HttpStatus.OK);
+            String productName = productRequest.getName();
+
+            // Check if the required fields are present in the request
+            if (productName == null) {
+                return new ResponseEntity<>("Product name is required.", HttpStatus.BAD_REQUEST);
+            }
+
+            List<Warehouse> warehouses = productRequest.getWarehouses(); // Extract the list of warehouses
+
+            for (Warehouse warehouse : warehouses) {
+                int warehouseId = warehouse.getId();
+                int quantity = productRequest.getQuantity(); // Get the quantity from the warehouse
+
+                // Generate the ID as per your logic
+                int generatedId = generateProductId();
+
+                // Call the AddProduct method for each warehouse
+                productRepository.AddProduct(generatedId, productName, "Dummy Description", quantity, warehouseId);
+            }
+
+            return new ResponseEntity<>("Product added successfully for all warehouses", HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+
+    // Generate product ID logic
+    private int generateProductId() {
+        // Implement your logic for generating a unique product ID
+        // This can be a simple auto-increment or any custom ID generation strategy
+        // Return the generated ID
+        return 0;
     }
 }
