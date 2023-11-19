187ef4b314133b657cab0a34dd990c7366bf8ee4
Junior Brito <junior.br.pr@gmail.com>
2023-11-19 22:57:46 +0100

    - Added project Repository and Controller. - Added getAllProjects endpoint

---
+++ backend/src/main/java/teamx/app/backend/rest/ProjectController.java
@@ -0,0 +1,40 @@
+package teamx.app.backend.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import teamx.app.backend.models.Project;
+import teamx.app.backend.repositories.ProjectRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/projects")
+public class ProjectController {
+    private final ProjectRepository projectRepository;
+
+    @Autowired
+    public ProjectController(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @GetMapping("/getAllProjects")
+    public ResponseEntity<List<Project>> getAllProjects() {
+        try {
+            List<Project> projects = new ArrayList<>(projectRepository.findAll());
+            if (projects.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.NO_CONTENT, "No projects found");
+            }
+            return ResponseEntity.ok(projectRepository.findAll());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
+    }
+}
