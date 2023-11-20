81df1bb4b5bb93373cf4f10af8f9ba6ee73d8756
Junior Brito <junior.br.pr@gmail.com>
2023-11-20 02:16:11 +0100

    - Can now retrieve current product stock from transactions

--- backend/src/main/java/teamx/app/backend/repositories/TransactionRepository.java
+++ backend/src/main/java/teamx/app/backend/repositories/TransactionRepository.java
@@ -3,5 +3,13 @@ package teamx.app.backend.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import teamx.app.backend.models.Transaction;
 
+import java.sql.Date;
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> getTransactionsByProjectId(Long projectId);
+    List<Transaction> getTransactionsByWarehouseId(Long warehouseId);
+    List<Transaction> getTransactionsByProductId(Long productId);
+    List<Transaction> getTransactionsByProductIdAndWarehouseIdAndTransactionDateBefore(Long productId, Long warehouseId
+    , Date transactionDateBefore);
 }
--- backend/src/main/java/teamx/app/backend/rest/TransactionController.java
+++ backend/src/main/java/teamx/app/backend/rest/TransactionController.java
@@ -3,14 +3,12 @@ package teamx.app.backend.rest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import teamx.app.backend.models.Transaction;
 import teamx.app.backend.repositories.TransactionRepository;
 
+import java.sql.Date;
 import java.util.List;
 
 @CrossOrigin(origins = "*")
@@ -32,6 +30,68 @@ public class TransactionController {
                 throw new ResponseStatusException(HttpStatus.NO_CONTENT, "No transactions found");
             }
             return new ResponseEntity<>(transactions, HttpStatus.OK);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error retrieving transactions");
+        }
+    }
+
+    @GetMapping(path = "/getTransactionByProjectId/{projectId}")
+    public ResponseEntity<List<Transaction>> getTransactionsByProjectId(@PathVariable Long projectId) {
+        try {
+            List<Transaction> transactions = transactionRepository.getTransactionsByProjectId(projectId);
+            if (transactions.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.NO_CONTENT, "No transactions found");
+            }
+            return new ResponseEntity<>(transactions, HttpStatus.OK);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error retrieving transactions");
+        }
+    }
+
+    @GetMapping(path = "/getTransactionByWarehouseId/{warehouseId}")
+    public ResponseEntity<List<Transaction>> getTransactionsByWarehouseId(@PathVariable Long warehouseId) {
+        try {
+            List<Transaction> transactions = transactionRepository.getTransactionsByWarehouseId(warehouseId);
+            if (transactions.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.NO_CONTENT, "No transactions found");
+            }
+            return new ResponseEntity<>(transactions, HttpStatus.OK);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error retrieving transactions");
+        }
+    }
+
+    @GetMapping(path = "/getTransactionsByProductId/{productId}")
+    public ResponseEntity<List<Transaction>> getTransactionsByProductId(@PathVariable Long productId) {
+        try {
+            List<Transaction> transactions = transactionRepository.getTransactionsByProductId(productId);
+            if (transactions.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.NO_CONTENT, "No transactions found");
+            }
+            return new ResponseEntity<>(transactions, HttpStatus.OK);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error retrieving transactions");
+        }
+    }
+
+    @GetMapping(path = "/getCurrentStock/{warehouseId}/{productId}")
+    public ResponseEntity<Integer> getCurrentStock(@PathVariable Long productId, @PathVariable Long warehouseId) {
+        try {
+            List<Transaction> transactions = transactionRepository.getTransactionsByProductIdAndWarehouseIdAndTransactionDateBefore(productId, warehouseId, new Date(System.currentTimeMillis()));
+            if (transactions.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.NO_CONTENT, "No transactions found");
+            }
+            int stock = 0;
+            for (Transaction transaction : transactions) {
+                if (transaction.getOrder() != null) {
+                    stock += transaction.getQuantity();
+                } else if (transaction.getTransferFrom() != null) {
+                    stock += transaction.getQuantity();
+                } else {
+                    stock -= transaction.getQuantity();
+                }
+            }
+            return new ResponseEntity<>(stock, HttpStatus.OK);
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error retrieving transactions");
         }
