9716605d71b7711de759b515dd830ed2a67d7377
aeons <jayden_gunhan@hotmail.com>
2023-11-21 03:27:22 +0100

    adjusted buttons

--- frontend/src/components/NavbarComponent.vue
+++ frontend/src/components/NavbarComponent.vue
@@ -20,10 +20,11 @@ export default {
 </script>
 
 <template>
-<!--<div v-if="session" class="d-flex flex-column flex-shrink-0 p-3 text-black bg-custom vh-100 w-auto sticky-top">-->
+  <!--<div v-if="session" class="d-flex flex-column flex-shrink-0 p-3 text-black bg-custom vh-100 w-auto sticky-top">-->
   <div class="d-flex flex-column flex-shrink-0 p-3 text-black bg-custom vh-100 w-auto sticky-top">
     <!-- Home Link with Icon -->
-    <router-link class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-black text-decoration-none" to="/dashboard">
+    <router-link class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-black text-decoration-none"
+                 to="/dashboard">
       <img :src="logo" alt="Your Company Logo" class="me-2 nav-height">
     </router-link>
 
@@ -68,19 +69,26 @@ export default {
       </li>
 
       <li class="nav-item dropdown">
-        <a class="nav-link text-black dropdown-toggle" href="#" id="adminDropdown" data-bs-toggle="dropdown" aria-expanded="false">
+        <a class="nav-link text-black dropdown-toggle" href="#" id="adminDropdown" data-bs-toggle="dropdown"
+           aria-expanded="false">
           Admin
         </a>
         <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="adminDropdown">
-          <li><router-link class="dropdown-item" to="/user">
-            User page
-          </router-link></li>
-          <li><router-link class="dropdown-item" to="/admin/products">
-            Products page
-          </router-link></li>
-          <li><router-link class="dropdown-item" to="/admin/project">
-            Projects
-          </router-link></li>
+          <li>
+            <router-link class="dropdown-item" to="/user">
+              User page
+            </router-link>
+          </li>
+          <li>
+            <router-link class="dropdown-item" to="/admin/products">
+              Products page
+            </router-link>
+          </li>
+          <li>
+            <router-link class="dropdown-item" to="/admin/project">
+              Projects
+            </router-link>
+          </li>
           <li><a class="dropdown-item" href="#">
             Administration
           </a></li>
@@ -88,9 +96,9 @@ export default {
       </li>
 
       <li class="nav-item" @click="logout">
-      <a class="nav-link text-black">
-        Log out
-      </a>
+        <a class="nav-link text-black">
+          Log out
+        </a>
       </li>
     </ul>
   </div>
@@ -112,14 +120,14 @@ export default {
   color: #000;
   background-color: #fff;
   border: 1px solid rgba(108, 117, 125, 0.45);
-  border-radius: 0.2rem;
+  border-radius: 0.375rem;
   box-shadow: 0 0 4px rgba(108, 117, 125, 0.35);
 }
 
 .nav-link.router-link-exact-active::after {
   background-color: var(--button-color);
   border: 1px solid #6c757d;
-  border-radius: 0.2rem;
+  border-radius: 0.375rem;
   box-shadow: 0 0 2px #6c757d;
   outline: black 1em;
 }
@@ -129,7 +137,8 @@ export default {
 }
 
 .bg-custom {
-  background: linear-gradient(
+  background:
+      linear-gradient(
       25deg,
       var(--gradient-color-start) 0%,
       var(--gradient-color-middle) 30%,
