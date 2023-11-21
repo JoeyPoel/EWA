a1186ad7ea9ddb7d13229dfe73c0b31ad52302bc
Junior Brito <junior.br.pr@gmail.com>
2023-11-21 18:03:07 +0100

    - Working

--- frontend/src/models/project.js
+++ frontend/src/models/project.js
@@ -1,6 +1,6 @@
 export class Project {
     constructor(id, name, description, location, clientName, clientEmail, clientPhone, startDate,
-                endDate, status) {
+                endDate, status, team, tasks, materials) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -11,6 +11,9 @@ export class Project {
         this.startDate = startDate;
         this.endDate = endDate;
         this.status = status;
+        this.team = team;
+        this.tasks = tasks;
+        this.materials = materials;
     }
 
     static statusList = [
