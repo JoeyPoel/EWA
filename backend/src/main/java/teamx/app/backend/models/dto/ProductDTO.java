b85d571f384371fa02c58bcb2d9b11adee1fc26b
Junior Brito Perez <junior.brito.perez@hva.nl>
2023-11-22 15:46:45 +0100

    Revert "- checkpoint"
    This reverts commit a85a476c4a46a6a8cb643a7d9812d0138ab0f4d0

--- backend/src/main/java/teamx/app/backend/models/dto/ProductDTO.java
+++ backend/src/main/java/teamx/app/backend/models/dto/ProductDTO.java
@@ -3,9 +3,11 @@ package teamx.app.backend.models.dto;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import teamx.app.backend.models.ProductCategory;
 
 @Data
 @NoArgsConstructor
--- backend/src/main/java/teamx/app/backend/services/TransactionService.java
+++ backend/src/main/java/teamx/app/backend/services/TransactionService.java
@@ -2,10 +2,7 @@ package teamx.app.backend.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import teamx.app.backend.models.Product;
 import teamx.app.backend.models.Transaction;
-import teamx.app.backend.models.dto.WarehouseProductDTO;
-import teamx.app.backend.repositories.ProductRepository;
 import teamx.app.backend.repositories.TransactionRepository;
 
 import java.sql.Date;
@@ -14,12 +11,10 @@ import java.util.List;
 @Service
 public class TransactionService {
     TransactionRepository transactionRepository;
-    ProductRepository productRepository;
 
     @Autowired
-    public TransactionService(TransactionRepository transactionRepository, ProductRepository productRepository) {
+    public TransactionService(TransactionRepository transactionRepository) {
         this.transactionRepository = transactionRepository;
-        this.productRepository = productRepository;
     }
 
     public List<Transaction> getAllTransactions() {
@@ -43,24 +38,37 @@ public class TransactionService {
     }
 
     public Integer getCurrentStock(Long warehouseId, Long productId) {
-        List<Integer> projectsProductsQuantitiesInFlow = transactionRepository.getAllByWarehouseIdAndTransactionFlowAndTransactionDateBefore(warehouseId, Transaction.Flow.IN, new Date(System.currentTimeMillis()));
-        List<Integer> projectsProductsQuantitiesOutFlow = transactionRepository.getAllByWarehouseIdAndTransactionFlowAndTransactionDateBefore(warehouseId, Transaction.Flow.OUT, new Date(System.currentTimeMillis()));
-        Integer currentStock = 0;
-        for (Integer quantity : projectsProductsQuantitiesInFlow) {
-            currentStock += quantity;
-        }
-        for (Integer quantity : projectsProductsQuantitiesOutFlow) {
-            currentStock -= quantity;
-        }
-        return currentStock;
+        List<Integer> projectsProductsQuantitiesInFlow = transactionRepository
+                .findTransactionQuantitiesByWarehouseIdAndProductIdAndTransactionFlowAndTransactionDateBefore(
+                warehouseId, productId, Transaction.Flow.IN, new Date(System.currentTimeMillis()));
+
+        List<Integer> transferInflow = transactionRepository
+                .findTransactionQuantitiesByWarehouseIdAndProductIdAndTransactionTypeAndTransactionDateBefore(
+                warehouseId, productId, Transaction.Type.TRANSFER, new Date(System.currentTimeMillis()));
+
+        System.out.println(transferInflow);
+        List<Integer> projectsProductsQuantitiesOutFlow = transactionRepository
+                .findTransactionQuantitiesByWarehouseIdAndProductIdAndTransactionFlowAndTransactionDateBefore(
+                warehouseId, productId, Transaction.Flow.OUT, new Date(System.currentTimeMillis()));
+
+        List<Integer> transferOutflowQuantity = transactionRepository
+                .findTransactionQuantitiesByTransferFromIdAndWarehouseIdAndTransactionDateBefore(
+                warehouseId, productId, new Date(System.currentTimeMillis()));
+
+
+        Integer totalInflow = projectsProductsQuantitiesInFlow.stream().mapToInt(Integer::intValue).sum() +
+                transferInflow.stream().mapToInt(Integer::intValue).sum();
+        Integer totalOutflow = projectsProductsQuantitiesOutFlow.stream().mapToInt(Integer::intValue).sum() +
+                transferOutflowQuantity.stream().mapToInt(Integer::intValue).sum();
+        return totalInflow - totalOutflow;
     }
 
-//    public List<WarehouseProductDTO> getWarehouseInventory(Long warehouseId) {
-//        List<Product> productList = productRepository.findAll();
-//
-//        List<WarehouseProductDTO> warehouseProductDTOs = transactionRepository.getWarehouseInventory(warehouseId);
-//        return warehouseProductDTOs;
-//    }
+    public List<Integer> getTransactionQuantitiesByWarehouseIdAndByProductIdAndTransactionTypeAndDateBetween(
+            Long warehouseId, Long productId, Transaction.Type transactionType, Date startDate, Date endDate) {
+        return transactionRepository
+                .findTransactionQuantitiesByWarehouseIdAndProductIdAndTransactionTypeAndTransactionDateBetween(
+                warehouseId, productId, transactionType, startDate, endDate);
+    }
 
     public Transaction getTransactionById(Long id) {
         return transactionRepository.findById(id).orElse(null);
