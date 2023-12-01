3f08296b969b457b216723531245017d604c2c38
Junior Brito <junior.br.pr@gmail.com>
2023-12-01 02:52:28 +0100

    Refactored projects

--- frontend/src/views/admin/projects/ProjectsComponent.vue
+++ frontend/src/views/admin/projects/ProjectsComponent.vue
@@ -1,112 +1,5 @@
 <template>
-  <!--  <div class="container mt-3">-->
-  <!--    <div class="d-flex justify-content-between mb-3">-->
-  <!--      <div class="col-md-4">-->
-  <!--        <input v-model="searchTerm" class="form-control" placeholder="Search for project..." type="text">-->
-  <!--      </div>-->
-  <!--      <div class="col-md-auto justify-content-end">-->
-  <!--        <button class="btn btn-success m-2" @click="sortProjectsByStatus">Sort by Status</button>-->
-  <!--        <button class="btn btn-primary m-2" data-bs-target="#projectModal" data-bs-toggle="modal" type="button"-->
-  <!--                @click="this.isAddingNewProject = true">-->
-  <!--          New Project-->
-  <!--        </button>-->
-  <!--      </div>-->
-  <!--    </div>-->
-
-
-  <!--    <ul class="list-group project-list">-->
-  <!--      <li-->
-  <!--          v-for="(project, index) in filteredProjects"-->
-  <!--          :key="index"-->
-  <!--          class="list-group-item d-flex justify-content-between align-items-start"-->
-  <!--          data-bs-target="#projectModal"-->
-  <!--          data-bs-toggle="modal"-->
-  <!--          @click="selectProject(project)"-->
-  <!--      >-->
-  <!--        <div class="ms-2 me-auto">-->
-  <!--          <div class="row">-->
-  <!--            <div class="col">-->
-  <!--              <h6 class="fw-bold">{{ project.name }}</h6>-->
-  <!--            </div>-->
-  <!--            <div class="row">-->
-  <!--              <p>Team: {{ project.team.name }}</p>-->
-  <!--            </div>-->
-  <!--            <div class="col text-end">-->
-  <!--              <div class="badge-column">-->
-  <!--                        <span class="badge badge-fixed-position position-md-static bg-info">{{-->
-  <!--                            getStatusDisplayName(project.status)-->
-  <!--                          }}</span>-->
-  <!--              </div>-->
-  <!--            </div>-->
-  <!--          </div>-->
-  <!--        </div>-->
-  <!--      </li>-->
-  <!--    </ul>-->
-
-
-  <!--    <div class="pt-3 fixed-bottom">-->
-  <!--      <button class="btn  btn-primary mb-3" data-bs-target="#projectModal " data-bs-toggle="modal" type="button"-->
-  <!--              @click="this.isAddingNewProject = true">-->
-  <!--        New Project-->
-  <!--      </button>-->
-  <!--    </div>-->
-
-  <!--    <div id="projectModal" aria-hidden="true" aria-labelledby="projectModalLabel" class="modal fade" tabindex="-1">-->
-  <!--      <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">-->
-  <!--        <div class="modal-content">-->
-  <!--          <div class="modal-header bg-white text-white">-->
-  <!--            <h5 class="modal-title text-dark">-->
-  <!--              {{ originalProject && originalProject.name ? originalProject.name : 'Untitled Project' }}</h5>-->
-  <!--            <button aria-label="Close" class="btn-close btn-close-dark" data-bs-dismiss="modal" type="button"-->
-  <!--                    @click="deselectProject"></button>-->
-  <!--          </div>-->
-
-  <!--          <div class="modal-body">-->
-  <!--            <div class="mb-3">-->
-  <!--              <h6 class="text">Name:</h6>-->
-  <!--              <input id="editName" v-model="editedProject.name" class="form-control" type="text">-->
-  <!--              <div v-if="!isValidProjectName" class="text-danger mt-1">Project name is required</div>-->
-  <!--            </div>-->
-  <!--            <div class="mb-3">-->
-  <!--              <h6 class="text">Status:</h6>-->
-  <!--              <select id="editStatus" v-model="editedProject.status" class="form-select">-->
-  <!--                <option v-for="statusOption in Project.statusList" :key="statusOption.value"-->
-  <!--                        :value="statusOption.value">-->
-  <!--                  {{ statusOption.displayName }}-->
-  <!--                </option>-->
-  <!--              </select>-->
-  <!--              <div v-if="!isValidProjectStatus" class="text-danger mt-1">Project status is required</div>-->
-  <!--            </div>-->
-  <!--            <div class="mb-3">-->
-  <!--              <h6 class="text">Assigned teams:</h6>-->
-  <!--              <select id="editTeams" v-model="editedProject.team" class="form-select">-->
-  <!--                <option v-for="team in teams" :key="team" :value="team">-->
-  <!--                  {{ team.name }}-->
-  <!--                </option>-->
-  <!--              </select>-->
-  <!--              <div v-if="!isValidProjectTeam" class="text-danger mt-1">Project team is required</div>-->
-  <!--            </div>-->
-  <!--            <div class="mb-3">-->
-  <!--              <h6 class="text">Description:</h6>-->
-  <!--              <textarea id="editDescription" v-model="editedProject.description" class="form-control"></textarea>-->
-  <!--            </div>-->
-  <!--            <div v-if="!isValidProjectDescription" class="text-danger mt-1">Project description is required</div>-->
-  <!--          </div>-->
-
-  <!--          <div class="modal-footer d-flex justify-content-start">-->
-  <!--            <button :disabled="!isValidProject()" class="btn btn-primary" data-bs-dismiss="modal" type="button"-->
-  <!--                    @click="saveProject">Save-->
-  <!--            </button>-->
-  <!--            <button aria-label="Delete" class="btn btn-danger" data-bs-dismiss="modal" type="button"-->
-  <!--                    @click="deleteProject">Delete-->
-  <!--            </button>-->
-  <!--            &lt;!&ndash;            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="deselectProject">Close</button>&ndash;&gt;-->
-  <!--          </div>-->
-  <!--        </div>-->
-  <!--      </div>-->
-  <!--    </div>-->
-  <!--  </div>-->
-  <v-container fluid>
+<v-container fluid>
     <base-card class="mt-1" color="secondary" title="Projects">
       <v-data-table
           v-model:items-per-page="itemsPerPage"
@@ -276,6 +169,7 @@ import {Project} from "@/models/Project.js";
 import BaseCard from "@/components/base/BaseCard.vue";
 
 export default {
+  // TODO: Fix date format for new and edit
   name: "ProjectListComponent",
   computed: {
     Project() {
@@ -455,29 +349,4 @@ export default {
 </script>
 
 <style scoped>
-.modal {
-  --bs-modal-header-border-color: transparent;
-}
-
-.modal-header {
-  background-color: #28a745;
-  color: white;
-}
-
-.list-group-item:hover {
-  cursor: pointer;
-  background-color: lightgreen;
-}
-
-.project-list {
-  max-height: 90vh;
-  overflow-y: auto;
-}
-
-.badge-column {
-  position: absolute;
-  bottom: 50px;
-  right: 50px;
-}
-
 </style>
