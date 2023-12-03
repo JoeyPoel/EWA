a16bee6e9d1f9b96c4cb1b59dd22fd99d054199f
Nizar Amine <aminen@hva.nl>
2023-12-03 17:01:29 +0100

    start component rework

--- frontend/src/views/admin/users/UsersComponent.vue
+++ frontend/src/views/admin/users/UsersComponent.vue
@@ -1,13 +1,147 @@
 <template>
-  <div>
-    users component
+  <div class="container mt-5">
+    <h1 class="text-center card text-light bg-dark mb-2 p-3">User List</h1>
+
+    <!-- "Create User" button -->
+    <button class="btn btn-dark mb-2" data-bs-toggle="modal"
+            data-bs-target="#createUserModal" @click="openCreateUserModal">Create User
+    </button>
+
+    <div class="row">
+      <div class="col-md-6" v-for="(user, index) in users" :key="index">
+        <div class="card mb-4">
+          <!-- Your card component -->
+          <div class="card-header d-flex justify-content-between align-items-center">
+            <h5 class="card-title">{{ user.name }}</h5>
+            <button class="btn btn-dark" @click="openCreateUserModal(user)">Update</button>
+          </div>
+          <div class="card-body">
+            <p class="card-text"><strong>Email:</strong> {{ user.email }}</p>
+            <p class="card-text"><strong>Team:</strong> {{ user.team.name }}</p>
+            <p class="card-text"><strong>Role:</strong> {{ user.role }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal for creating a new user -->
+    <div class="modal fade" id="createUserModal" ref="createUserModal" tabindex="-1" aria-labelledby="projectModalLabel"
+         aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+
+        <div class="modal-content">
+
+          <div class="modal-header bg-dark text-white">
+
+            <h5 class="modal-title">Create User</h5>
+
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"
+                    @click="closeCreateUserModal"></button>
+
+          </div>
+
+          <div class="modal-body">
+            <!-- Form to fill in name, email, role, and team for creating a new user -->
+
+            <form @submit.prevent="createUser">
+
+              <div class="mb-3">
+                <label for="newUserName" class="form-label">Name</label>
+                <input type="text" class="form-control" id="newUserName" v-model="newUser.name" required>
+              </div>
+
+              <div class="mb-3">
+                <label for="newUserEmail" class="form-label">Email</label>
+                <input type="email" class="form-control" id="newUserEmail" v-model="newUser.email" required>
+              </div>
+
+              <div class="mb-3">
+                <label for="newUserRole" class="form-label">Role</label>
+                <select class="form-control" id="newUserRole" v-model="newUser.role" required>
+                  <option>USER</option>
+                  <option>ADMIN</option>
+                </select>
+              </div>
+
+              <div class="mb-3">
+                <label for="newUserTeam" class="form-label">Team</label>
+                <select id="editTeams" v-model="newUser.team" class="form-select">
+                  <option v-for="team in teams" :key="team.id" :value="team">{{ team.name }}</option>
+                </select>
+              </div>
+
+              <button type="submit" class="btn btn-dark" data-bs-toggle="modal"
+                      data-bs-target="#createUserModal">Create
+              </button>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup>
+<script>
 
+export default {
+  name: "UserComponent",
+  inject: ['usersService', 'teamsService'],
+  data() {
+    return {
+      teams: [],
+      users: [],
+      newUser: {
+
+      },
+      isCreateUserModalOpen: false,
+    };
+  },
+  async mounted() {
+    this.teams = await this.teamsService.asyncFindAll();
+  },
+  async created() {
+    console.log("test")
+    this.users = await this.usersService.asyncFindAll();
+    console.log(this.users)
+
+
+  },
+  methods: {
+    async openCreateUserModal() {
+      this.newUser = {
+
+      };
+      this.isCreateUserModalOpen = true;
+    },
+    closeCreateUserModal() {
+      this.isCreateUserModalOpen = false;
+    },
+    async createUser() {
+      // Validate the form fields (add additional validation as needed)
+      if (!this.newUser.name || !this.newUser.email || !this.newUser.role) {
+        alert("Please fill in all fields.");
+        return;
+      }
+
+      // Create a new object with all properties of newUser except team
+      const userToSave = { ...this.newUser };
+
+      console.log(userToSave)
+      const savedUser = await this.usersService.asyncSave(userToSave);
+      if (savedUser) {
+        this.users.push(savedUser);
+        this.closeCreateUserModal();
+      } else {
+        alert("Failed to create user. Please try again.");
+      }
+    },
+  }
+};
 </script>
 
 <style scoped>
-
+.modal {
+  pointer-events: none; /* Prevent interaction with hidden elements */
+}
 </style>
